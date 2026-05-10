f91d6877b8b22f5f9215b82d6323cbb165fa6538
Pixxie Jinx <leonrosaparlante1@gmail.com>
2026-05-10 22:06:14 +0200

    Servidor básico de Websockets

---
+++ config/server_config.ts
@@ -0,0 +1,7 @@
+
+
+export const SERVER_CONFIG = {
+    PORT: Number (process.env.PORT) || 3000,
+    idleTimeout: 30, // Tiempo de espera para conexiones inactivas (en milisegundos)
+    path: "/socket.io/", // Ruta para la conexión de Socket.IO
+};
